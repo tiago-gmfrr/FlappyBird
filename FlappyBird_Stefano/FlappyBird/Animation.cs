fb37abf652212bf3cf8cce8b3d1c3ddf74b55b5f
tiago-gmfrr <tiago.gmfrr@eduge.ch>
2019-11-26 13:00:02 +0100

    first commit

---
+++ FlappyBird_Stefano/FlappyBird/Animation.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace FlappyBird
+{
+    class Animation
+    {
+        #region vars
+        // The image representing the collection of images used for animation
+        public Texture2D texture;
+
+        // The scale used to display the sprite strip
+        float scale;
+
+        // The time since we last updated the frame
+        int elapsedTime;
+
+        // The time we display a frame until the next one
+        int frameTime;
+
+        // The number of frames that the animation contains
+        int frameCount;
+
+        // The index of the current frame we are displaying
+        int currentFrame;
+
+        // The color of the frame we will be displaying
+        Color color;
+
+        // The area of the image strip we want to display
+        Rectangle sourceRect = new Rectangle();
+
+        // The area where we want to display the image strip in the game
+        Rectangle destinationRect = new Rectangle();
+
+        // Width of a given frame
+        public int frameWidth;
+
+        // Height of a given frame
+        public int frameHeight;
+
+        // The state of the Animation
+        public bool Active;
+
+        // Determines if the animation will keep playing or deactivate after one run
+        public bool Looping;
+
+        // Width of a given frame
+        public Vector2 pos;
+        #endregion
+
+        public void Initialize(Texture2D texture, Vector2 position,
+            int frameWidth, int frameHeight, int frameCount, int frameTime,
+            Color color, float scale, bool looping)
+        {
+            // Keep a local copy of the values passed in
+            this.color = color;
+            this.frameWidth = frameWidth;
+            this.frameHeight = frameHeight;
+            this.frameCount = frameCount;
+            this.frameTime = frameTime;
+            this.scale = scale;
+
+            Looping = looping;
+            pos = position;
+            this.texture = texture;
+
+            // Set the time to zero
+            elapsedTime = 0;
+            currentFrame = 0;
+
+            // Set the Animation to active by default
+            Active = true;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            // Do not update the game if we are not active
+            if (Active == false) return;
+
+            // Update the elapsed time
+            elapsedTime += gameTime.ElapsedGameTime.Milliseconds;
+
+            // If the elapsed time is larger than the frame time
+            // we need to switch frames
+            if (elapsedTime > frameTime)
+            {
+                // Move to the next frame
+                currentFrame++;
+
+                // If the currentFrame is equal to frameCount reset currentFrame to zero
+                if (currentFrame == frameCount)
+                {
+                    currentFrame = 0;
+
+                    // If we are not looping deactivate the animation
+                    if (Looping == false)
+                        Active = false;
+                }
+
+                // Reset the elapsed time to zero
+                elapsedTime = 0;
+            }
+
+            // Grab the correct frame in the image strip by multiplying the currentFrame index by the Frame width 
+            sourceRect = new Rectangle(currentFrame * frameWidth, 0, frameWidth, frameHeight);
+
+            // Grab the correct frame in the image strip by multiplying the currentFrame index by the frame width
+            destinationRect = new Rectangle((int)pos.X, (int)pos.Y,
+                (int)(frameWidth * scale), (int)(frameHeight * scale));
+        }
+
+        public void Draw(SpriteBatch spriteBatch, float rotate, Vector2 origin)
+        {
+            // Only draw the animation when we are active
+            if (Active)
+                spriteBatch.Draw(texture, destinationRect, sourceRect, color, rotate, origin, SpriteEffects.None, 0);
+        }
+    }
+}
---
+++ FlappyBird_Stefano/FlappyBird/DoublePipe.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace FlappyBird
+{
+    class DoublePipe
+    {
+        Vector2 pos;
+
+        Texture2D texture;
+
+        Pipe down, up;
+
+        public static int speed = 7, offset = 550;
+
+        public bool isPointAdded = false;
+
+        Vector2 posUp => pos - new Vector2(0, offset);
+        Vector2 posDown => pos + new Vector2(0, offset);
+
+        Rectangle rectUp =>
+            new Rectangle((int)posUp.X, (int)posUp.Y - texture.Height / 2, texture.Width, texture.Height);
+
+        Rectangle rectDown =>
+            new Rectangle((int)posDown.X, (int)posDown.Y - texture.Height / 2, texture.Width, texture.Height);
+
+        public float X => pos.X;
+
+        public DoublePipe(Texture2D texture, Vector2 pos)
+        {
+            this.texture = texture;
+            this.pos = pos;
+
+            up = new Pipe(texture);
+            down = new Pipe(texture);
+        }
+
+        public void Update() => pos.X -= speed;
+
+        public void Draw(SpriteBatch sb)
+        {
+            up.Draw(sb, 0, posUp, SpriteEffects.None);
+            down.Draw(sb, MathHelper.Pi, posDown, SpriteEffects.FlipHorizontally);
+        }
+
+        public bool IsOut => pos.X + texture.Width < 0;
+
+        public bool IsCollinding(Rectangle hitbox)
+            => rectUp.Intersects(hitbox) || rectDown.Intersects(hitbox);
+    }
+}
